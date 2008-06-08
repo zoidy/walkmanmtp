ca4fd10a19d9ac4f9c78647f4f53b2f9726cdd28
rios.fernando <rios.fernando@a5a30751-4a49-0410-9d40-f573051187d2>
2008-06-08 21:03:30 +0000

    0.4.1 - 0.4 -settings are now saved (window state etc)

--- WalkmanMTP/DeviceDetails.vb
+++ WalkmanMTP/DeviceDetails.vb
@@ -1,4 +1,19 @@
-﻿Public Class DeviceDetails
+﻿''Copyright 2008 Dr. Zoidberg
+' 
+'Licensed under the Apache License, Version 2.0 (the "License");
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the License at
+'
+'	http://www.apache.org/licenses/LICENSE-2.0 
+'
+'Unless required by applicable law or agreed to in writing, software 
+'distributed under the License is distributed on an "AS IS" 
+'BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'See the License for the specific language governing permissions 
+'and limitations under the License. 
+
+
+Public Class DeviceDetails
 
 
     Private Sub DeviceDetails_VisibleChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.VisibleChanged
